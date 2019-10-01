834397278ef4036f9d2a2b2fa477dd64bab002f1
Damien Meere <damien.meere@bt.com>
2019-10-01 15:52:59 +0000

    removed setData command, and employed a timeout to hold off on console for 5000ms

--- main.js
+++ main.js
@@ -5,20 +5,14 @@ var data;
 xhr.open("GET", "https://swapi.co/api/");
 xhr.send();
 
-function setData(jsonData) {
-    data = jsonData;
-}
-
 xhr.onreadystatechange = function() {
     // for information on the XHR Ready States - https://developer.mozilla.org/en-US/docs/Web/API/XMLHttpRequest/readyState
     // 4 = Operation is complete
     console.log(this.readyState);
     if (this.readyState == 4 && this.status == 200) {
-        setData(JSON.parse(this.responseText));
-
         // Parse the JSON Object and log to the console
         //console.log(JSON.parse(this.responseText));
-        //data = this.responseText;
+        data = this.responseText;
         //retrieve data and place it in the index.html data placeholder
         //document.getElementById("data").innerHTML = this.responseText;
     }
