a9d2e85542c387bc907c2972b246526679c2f376
Damien Meere <damien.meere@bt.com>
2019-10-06 11:25:59 +0000

    using key value pairs to place data within table structure

--- main3.js
+++ main3.js
@@ -14,8 +14,33 @@ function getData(type, cb) {
 }
 
 function writeToDocument(type) {
+    var tableRows = [];
+    // This clears the HTML data element each time a button is clicked
+    var el = document.getElementById("data");
+    el.innerHTML = "";
+
     getData(type, function(data) {
-        console.dir(data);
-        document.getElementById("data").innerHTML = data.results;
+        data = data.results;
+        var tableHeaders = getTableHeaders(data[0]);
+
+        data.forEach(function(item){
+            var dataRow = [];
+            Object.keys(item).forEach(function(key){
+                dataRow.push(`<td>{$item[key]}</td>`);
+            });
+            tableRows.push(dataRow);
+        });
+        el.innerHTML = `<table${tableHeaders}${tableRows}</table>`
     });
+}
+
+function getTableHeaders(obj){
+    var tableHeaders = [];
+    Object.keys(obj).forEach(function(key){
+        // <td> creates a new table cell.
+        // Using Backtick here, this is called a template literal, which allows us to interpolate variables and strings like this
+        tableHeaders.push(`<td>${key}</td>`);
+    });
+    // <tr> adds the value to a row
+    return `<tr>${tableHeaders}</tr>`;
 }
